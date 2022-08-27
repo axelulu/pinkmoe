00b27181bbf0578ed3ada5281c444e614b9ee744
coderzhaolu <izhaicy@163.com>
2022-08-27 16:28:31 +0800

    seo

--- pinkmoe_index/components/Header/index.vue
+++ pinkmoe_index/components/Header/index.vue
@@ -2,7 +2,7 @@
  * @Author: coderzhaolu && izhaicy@163.com
  * @Date: 2022-07-18 21:44:07
  * @LastEditors: coderzhaolu && izhaicy@163.com
- * @LastEditTime: 2022-08-27 11:24:24
+ * @LastEditTime: 2022-08-27 16:26:28
  * @FilePath: /pinkmoe_index/components/Header/index.vue
  * @Description: https://github.com/Coder-ZhaoLu/pinkmoe   (如需用于商业用途或者二开，请联系作者捐助任意金额即可)
  * QQ:2419857357;支付宝:13135986153
@@ -37,9 +37,9 @@ const {
 
 <template>
   <div
-    class="h-48 bg-blue-200 w-full flex justify-end items-center flex-col bg-cover"
-    :style="`background-image: url(${appStore.siteBasic?.background});`"
+    class="h-48 bg-blue-200 w-full flex justify-end items-center flex-col relative"
   >
+    <img v-if="appStore.siteBasic?.background?.[0]" class="absolute top-0 left-0 h-48 bg-cover w-full animate-lazyloaded" :src="appStore.siteBasic?.background?.[0]" alt="">
     <div class="w-full z-10 h-full">
       <div class="mb-2 lg:w-3/4 xl:w-5/12 h-32 m-auto animate-fadeIn30">
         <div
