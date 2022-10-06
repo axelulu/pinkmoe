cc8b3b1dac506c10c15a2340277802ef7bd93556
Coder-ZhaoLu <izhaicy@163.com>
2022-10-06 10:46:26 +0800

    更新版权信息

--- pinkmoe_index/api/upload/index.ts
+++ pinkmoe_index/api/upload/index.ts
@@ -4,9 +4,11 @@
  * @LastEditors: coderzhaolu && izhaicy@163.com
  * @LastEditTime: 2022-08-07 09:09:24
  * @FilePath: /pinkmoe_index/src/api/upload/index.ts
- * @Description: https://github.com/Coder-ZhaoLu/pinkmoe   (如需用于商业用途或者二开，请联系作者捐助任意金额即可)
- * QQ:2419857357;支付宝:13135986153
- * Copyright (c) 2022 by coderzhaolu, All Rights Reserved.
+ * @Description: https://github.com/Coder-ZhaoLu/pinkmoe 
+ * 问题反馈qq群:749150798
+ * PinkMoe主题上所有内容(包括但不限于 文字，图片，代码等)均为指针科技原创所有，采用请注意许可
+ * 请遵循 “非商业用途” 协议。商业网站或未授权媒体不得复制内容，如需用于商业用途或者二开，请联系作者捐助任意金额即可，我们将保存所有权利。
+ * Copyright (c) 2022 by 指针科技, All Rights Reserved.
  */
 // 权限问题后期增加
 import { get, post } from '/@/utils/http/axios'
--- pinkmoe_index/pages/user-center/settings/index.vue
+++ pinkmoe_index/pages/user-center/settings/index.vue
@@ -4,9 +4,11 @@
  * @LastEditors: coderzhaolu && izhaicy@163.com
  * @LastEditTime: 2022-09-10 16:50:29
  * @FilePath: /pinkmoe_index/pages/user-center/settings/index.vue
- * @Description: https://github.com/Coder-ZhaoLu/pinkmoe   (如需用于商业用途或者二开，请联系作者捐助任意金额即可)
- * QQ:2419857357;支付宝:13135986153
- * Copyright (c) 2022 by coderzhaolu, All Rights Reserved.
+ * @Description: https://github.com/Coder-ZhaoLu/pinkmoe 
+ * 问题反馈qq群:749150798
+ * PinkMoe主题上所有内容(包括但不限于 文字，图片，代码等)均为指针科技原创所有，采用请注意许可
+ * 请遵循 “非商业用途” 协议。商业网站或未授权媒体不得复制内容，如需用于商业用途或者二开，请联系作者捐助任意金额即可，我们将保存所有权利。
+ * Copyright (c) 2022 by 指针科技, All Rights Reserved.
 -->
 <script lang="ts" setup name="UserCenterSettingsIndex">
 import UserCenterLayout from '/@/components/Usercenterlayout/index.vue'
